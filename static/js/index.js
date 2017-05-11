981a118d6eca7e3de30255dc3c17d4e1d1ffbd89
joassouza <joassouzasantos@gmail.com>
2017-05-11 20:14:20 -0300

    [fix] Paste text updated of comment edited
    This commit fixes the scenario of not pasting the comment text updated
    when user edits a comment then copies it.

--- static/js/index.js
+++ static/js/index.js
@@ -205,6 +205,10 @@ ep_comments.prototype.init = function(){
         $commentBox.children('.comment-edit-form').remove();
         $commentBox.children('.comment-author-name, .comment-text').removeClass('hidden');
         self.updateCommentBoxText(commentId, commentText);
+
+        // although the comment or reply was saved on the data base successfully, it needs
+        // to update the comment or comment reply variable with the new text saved
+        self.setCommentOrReplyNewText(commentId, commentText);
       }
     });
   });
@@ -816,6 +820,15 @@ ep_comments.prototype.setCommentReply = function(commentReply){
   var replyId = commentReply[0];
   if(commentReplies[replyId] == null) commentReplies[replyId] = {}
   commentReplies[replyId] = commentReply[1];
+};
+
+// set the text of the comment or comment reply
+ep_comments.prototype.setCommentOrReplyNewText = function(commentOrReplyId, text){
+  if(this.comments[commentOrReplyId]){
+    this.comments[commentOrReplyId].data.text = text;
+  }else if(this.commentReplies[commentOrReplyId]){
+    this.commentReplies[commentOrReplyId].text = text;
+  }
 };
 
 // Get all comments
