3bd39f45a98a6c87639d548a4856dc736e8db3ca
Guilherme Goncalves <inacio.guilherme@gmail.com>
2020-05-25 16:50:26 -0300

    [refactor] Does not handle shortcuts for non-ScriptDocument pads

--- static/js/shortcuts.js
+++ static/js/shortcuts.js
@@ -1,9 +1,14 @@
 var eascUtils = require('ep_script_toggle_view/static/js/utils');
 var SHORTCUT_BASE = require('ep_script_scene_marks/static/js/constants').TEKSTO_SHORTCUT_BASE;
+var utils = require('./utils');
 
 var SHORTCUT_KEY = 'C';
 
 exports.processAceKeyEvent = function(context) {
+  // handles key events only in ScriptDocument pads
+  var isScriptDocumentPad = pad.plugins.ep_comments_page.padType.isScriptDocumentPad();
+  if (!isScriptDocumentPad) return false;
+
   var evt = context.evt;
   var key = String.fromCharCode(evt.which);
 
